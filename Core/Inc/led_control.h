e1cddf8abaade374b9276505f5b26f21fa550abe
Matej Planinsek <matej.planinsek@gmail.com>
2023-07-19 11:47:15 +0200

    Improved macros naming in fec. Added functions to set LED current in ledctrl.

--- Core/Inc/led_control.h
+++ Core/Inc/led_control.h
@@ -8,11 +8,21 @@
 #include "stm32g4xx_hal.h"
 #include "dac.h"
 #include "main.h"
+#include "debug.h"
 
 #define LEDCTRL_DAC_HANDLE &hdac1
 #define LEDCTRL_DAC_CH DAC_CHANNEL_1
+#define LEDCTRL_DAC_MAX_RAW 4095
+#define LEDCTRL_VREF 3.0f
+#define LEDCTRL_ZERO_CURRENT_CTRL 0.0927f
+//current gain in V/A starting from 92.7mV offset
+#define LEDCTRL_CURRENT_GAIN 1.88f
+#define LED_CURRENT_MAX 1.5f
 
 void ledctrl_init(void);
 void ledctrl_set_dac_raw(uint32_t dac_value);
+uint32_t ledctrl_get_raw_from_current(float current);
+void ledctrl_set_current(float current);
+
 
 #endif //LIGHTSOAKFW_STM_LED_CONTROL_H
--- Core/Src/led_control.c
+++ Core/Src/led_control.c
@@ -16,6 +16,64 @@ void ledctrl_init(void){
 
 }
 
+/**
+ * @brief set raw value to DAC
+ * @param dac_value raw DAC value
+ */
 void ledctrl_set_dac_raw(uint32_t dac_value){
   HAL_DAC_SetValue(LEDCTRL_DAC_HANDLE, LEDCTRL_DAC_CH, DAC_ALIGN_12B_R, dac_value);
 }
+
+/**
+ * @brief Calculate raw DAC value from desired current.
+ * Do this in advance and set dac with ledctrl_set_dac_raw() at the exact time.
+ * @param current current in A
+ * @return raw DAC value to get this LED current
+ */
+uint32_t ledctrl_get_raw_from_current(float current){
+  // Check if current is within the range
+  if (current > LED_CURRENT_MAX) {
+    current = LED_CURRENT_MAX;
+    dbg(Error, "Current request too high! Setting to max!\n\r");
+  }
+  else if (current < 0) {
+    current = 0;
+    dbg(Error, "Current request below 0! Setting to 0!\n\r");
+  }
+
+  //if 0 requested, force control voltage to 0, not LEDCTRL_ZERO_CURRENT_CTRL
+  if(current == 0.0f){
+    return 0;
+  }
+
+  // Calculate control voltage
+  float control_voltage = (current * LEDCTRL_CURRENT_GAIN) + LEDCTRL_ZERO_CURRENT_CTRL;
+
+  // Calculate corresponding raw value
+  uint32_t dac_raw_val = (uint32_t) ((control_voltage / LEDCTRL_VREF) * LEDCTRL_DAC_MAX_RAW);
+
+  // Check if the value is within the DAC range
+  if (dac_raw_val > LEDCTRL_DAC_MAX_RAW) {
+    dac_raw_val = LEDCTRL_DAC_MAX_RAW;
+  }
+
+  return dac_raw_val;
+}
+
+
+/**
+ * @brief Set LED current. This function will calculate raw DAC value and set it.
+ * Do not use if timing is critical.
+ * @param current current in A
+ */
+void ledctrl_set_current(float current){
+  //no need to calculate if 0 requested. Set to 0, not LEDCTRL_ZERO_CURRENT_CTRL to get 100% turn off
+  if(current == 0){
+    ledctrl_set_dac_raw(0);
+    return;
+  }
+  uint32_t dac_raw_val = ledctrl_get_raw_from_current(current);
+  ledctrl_set_dac_raw(dac_raw_val);
+}
+
+
