2266bcda19c80da65de2c2b02af1598528ff44cb
Matija Pirc <matic.pirc@gmail.com>
2024-12-17 11:00:28 +0100

    Increased required time for MPPT to 21ms, because averaging current over 20ms significantly reduces noise in the current measurements, significantly improving the reliability of MPPT at low irradiance.

--- Core/Inc/measurements.h
+++ Core/Inc/measurements.h
@@ -36,7 +36,7 @@
 #define MEAS_IV_CHAR_MIN_STEPS_THR 3 //
 #define MEAS_IV_CHAR_MIN_DELTA_V 0.002 //V  //PWM ripple up to 0.62mV and step size 1.6 mV
 
-#define MPPT_DURATION 2000  //us
+#define MPPT_DURATION 21000  //us
 #define MPPT_VOLTAGE_STEP 0.020 //0.002 V
 #define MPPT_SEARCH_VOLTAGE_STEP 0.10 //V
 #define MPPT_SETTLING_TIME_DEFAULT  100000  //us
